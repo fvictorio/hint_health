2ea5e2fa8160b66f9b61d9542c69354246861274
Franco Victorio <victorio.franco@gmail.com>
2021-01-06 20:35:00 -0300

    Suggestions

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const axios = require('axios').default;
 module.exports = class HintClient {
   constructor(client_key, password) {
-    this.client_key = client_key;
+    this.client_key = client_key; // snake_case is not used in js
     this.password = password
     this.baseUri = 'https://api.hint.com/api';
   }
 
   credentials() {
+    // it's not very idiomatic to do return with parentheses, just do return { ... }
     return({ auth: { username: this.client_key, pass: this.password } })
   };
 
+  // mixing async/await and .then is weird. This function is rewritten to only use async/await,
+  // the next one is rewritten to only use promises
+  // I prefer this one, but YMMV
   async deleteCard(params){
-    let hintResponse = await axios.delete(`${this.baseUri}/provider/patients/${params.patientId}/cards/${params.id}`, this.credentials())
-      .then(function (response) {
-        console.log(`STATUS: ${response.status}`)
-        return({ data: response.data, status: response.status })
-      })
-      .catch(function (error) {
-        console.log(`STATUS: ${error.response.status}`)
-        return({ data: error.response.data, status: error.response.status })
-      });
-    return(hintResponse);
+    try {
+      const response = await axios.delete(`${this.baseUri}/provider/patients/${params.patientId}/cards/${params.id}`, this.credentials())
+      console.log(`STATUS: ${response.status}`)
+      return { data: response.data, status: response.status }
+    } catch (error) {
+      console.log(`STATUS: ${error.response.status}`)
+      return { data: error.response.data, status: error.response.status }
+    };
   };
 
-  async createCard(params){
-    let hintResponse = await axios.post(`${this.baseUri}/provider/patients/${params.patient_id}/cards`, params, this.credentials())
+  createCard(params){
+    return axios.post(`${this.baseUri}/provider/patients/${params.patient_id}/cards`, params, this.credentials())
       .then(function (response) {
         console.log(`STATUS: ${response.status}`)
         return({ data: response.data, status: response.status })
@@ -33,7 +35,6 @@ module.exports = class HintClient {
         console.log(`STATUS: ${error.response.status}`)
         return({ data: error.response.data, status: error.response.status })
       });
-    return(hintResponse);
   };
 
   async deleteMembership(params){
